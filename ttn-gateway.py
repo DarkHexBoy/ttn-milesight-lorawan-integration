7c1168b5208f505816d0a732ffcc1362ca72313c
Milesight-Lockon <lockon.wen@milesight.com>
2025-04-25 16:25:13 +0800

    Update ttn-gateway.py

--- ttn-gateway.py
+++ ttn-gateway.py
@@ -53,22 +53,22 @@ if response.status_code in [200, 201]:
     print(response.json())
 
     # === 根据 frequency_plan_id 输出正确的 Semtech 地址 ===
-    frequency_lns_map = {
-        "EU_863_870": "eu1",
-        "US_902_928": "nam1",  # 对应 US915
-        "US_902_928_FSB_2": "nam1",  # 对应 US915
-        "AU_915_928": "au1",
-        "AS_923": "as1",
-        "AS_923_1": "as1",
-        "CN_470_510": "cn1",
-        "IN_865_867": "in1",
-        "KR_920_923": "kr1",
-        "RU_864_870": "eu1",
-        "RS_865_867": "eu1"
-    }
+    # frequency_lns_map = {
+    #     "EU_863_870": "eu1",
+    #     "US_902_928": "nam1",  # 对应 US915
+    #     "US_902_928_FSB_2": "nam1",  # 对应 US915
+    #     "AU_915_928": "au1",
+    #     "AS_923": "as1",
+    #     "AS_923_1": "as1",
+    #     "CN_470_510": "cn1",
+    #     "IN_865_867": "in1",
+    #     "KR_920_923": "kr1",
+    #     "RU_864_870": "eu1",
+    #     "RS_865_867": "eu1"
+    # }
 
     # 自动匹配区域前缀
-    lns_region = frequency_lns_map.get(frequency_plan_id, "eu1")  # 默认 fallback 为 "eu1"
+    # lns_region = frequency_lns_map.get(frequency_plan_id, "eu1")  # 默认 fallback 为 "eu1"
     
     semtech_address = f"{base_url.replace('https://', '')}:1700"
     print(f"\n🌐 Semtech 地址是：{semtech_address}")
